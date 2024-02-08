8f3bc60425897f07fcd34438dc7e59d98a8e070f
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-02-08 23:26:51 +0100

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors : {
+      'primary-color' : '#23A6F0',
+      'danger-color' : '#E74040',
+      'second-text-color' : '#737373',
+      'light-gray-color' : '#FAFAFA',
+    },
+    extend: {
+      fontFamily :  {
+        'sans' : ['sans-serif', 'Monstserrat'],
+      }
+     
+    },
+  },
+  plugins: [],
+}
